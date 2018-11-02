937111205b70302cf4cc21a13684c9c8ce350ac7
Uwe Michael Scheffer <uwe.scheffer@scheffer-software.de>
2018-11-02 13:05:09 +0100

    Prompt user for some input (and return the result)

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 let Phrase = require("mhartl-palindrome");
 
-// First palindrome proof-of-concept.
-alert(new Phrase("Madam, I'm Adam.").palindrome());
+// Prompt user for some input.
+let string = prompt("Please enter a string for palindrome testing:");
+let phrase = new Phrase(string);
+
+// Return the result.
+if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome!`);
+} else {
+    alert(`"${phrase.content}" is not a palindrome.`);
+}
 
